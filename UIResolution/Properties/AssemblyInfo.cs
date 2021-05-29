2d7af5c1159143660d82691abb5cfa15beff4bcc
MacSergey <macsergey@me.com>
2021-05-29 19:27:43 +0300

    update common

--- UIResolution/Properties/AssemblyInfo.cs
+++ UIResolution/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("6286e108-7c7c-472a-a502-2de06d3a92d0")]
 
 #if BETA
-[assembly: AssemblyVersion("1.0.0.182")]
-[assembly: AssemblyFileVersion("1.0.0.182")]
+[assembly: AssemblyVersion("1.0.0.185")]
+[assembly: AssemblyFileVersion("1.0.0.185")]
 #else
 [assembly: AssemblyVersion("1.1.1")]
 [assembly: AssemblyFileVersion("1.1.1")]
