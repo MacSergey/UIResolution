486c6ba73486a222907ee4c8350d9616c5e52d0b
MacSergey <macsergey@me.com>
2021-08-01 13:48:53 +0300

    Added Korean translation

--- UIResolution/Properties/AssemblyInfo.cs
+++ UIResolution/Properties/AssemblyInfo.cs
@@ -15,9 +15,9 @@ using System.Runtime.InteropServices;
 [assembly: Guid("6286e108-7c7c-472a-a502-2de06d3a92d0")]
 
 #if BETA
-[assembly: AssemblyVersion("1.0.0.196")]
-[assembly: AssemblyFileVersion("1.0.0.196")]
+[assembly: AssemblyVersion("1.0.0.198")]
+[assembly: AssemblyFileVersion("1.0.0.198")]
 #else
-[assembly: AssemblyVersion("1.1.1.4")]
-[assembly: AssemblyFileVersion("1.1.1.4")]
+[assembly: AssemblyVersion("1.1.2")]
+[assembly: AssemblyFileVersion("1.1.2")]
 #endif
